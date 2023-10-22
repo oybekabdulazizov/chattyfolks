c4cfb06049ddae4c2eee766db75879239baa8440
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-10-22 13:39:44 +0200

    some margin change in styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='max-w-[600px] lg:mx-auto mx-6 flex flex-col gap-6 py-6 text-start'>
+    <div className='max-w-[600px] lg:mx-auto mx-auto flex flex-col gap-6 py-6 text-start'>
       <h1 className='text-4xl block'>ChattyFolks</h1>
       <AddPostForm />
       <PostsList />
